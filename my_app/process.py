bf980f619ca5402ddc01165465ec92c27f011b9c
Rohit Sharma <rohitsrmaofficial@gmail.com>
2024-06-21 00:13:07 +0530

    Some changes

--- my_app/process.py
+++ my_app/process.py
@@ -4,7 +4,5 @@ from .models import Campaign
 def send_email(modeladmin, request, queryset):
     # Iterate through the selected campaigns and send emails
     for campaign in queryset:
-        # You can add your email sending logic here
-        # For example, you can call your `send_email` function from here
         send_email(campaign)
 send_email.short_description = "send email"
